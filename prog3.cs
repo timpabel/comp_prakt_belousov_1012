ff8303d167f5e0c66178eec95cac3dc9786a3daf
timpabel <belousov.tp@gmail.com>
2025-02-18 21:44:50 +0300

    6 номер

---
+++ prog3.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    class prog3
+    {
+        static void Main(string[] args)
+            {
+                string num = Console.ReadLine();
+                char firstDigit = num[0];
+
+                bool flag = true;
+                foreach (char digit in num)
+                {
+                    if (digit != firstDigit)
+                    {
+                        flag = false;
+                        break;
+                    }
+                }
+
+                if (flag)
+                {
+                    Console.WriteLine("Да");
+                }
+                else
+                {
+                    Console.WriteLine("Нет");
+                }
+            }
+        }
+    }
+ 
