e98b56f476f66bf57ad8991cc8a75f8d78eabb02
timpabel <belousov.tp@gmail.com>
2025-02-18 21:35:34 +0300

    Add files via upload

---
+++ Class1.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    class Program1
+    {
+        static void Main(string[] args)
+        {
+            int num = int.Parse(Console.ReadLine());
+
+            for (int i = 2; i <= num; i++)
+            {
+                if (num % i == 0)
+                {
+                    Console.WriteLine("наименьший делитель = "+i);
+                    break;
+                }
+            }
+        }
+    }
+}
